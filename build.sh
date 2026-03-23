80550e0730d0e7eea3b3f367a62c2b6f8752e336
Mark Joseph <imrkjoseph16@Mjs-MacBook-Air-M3.local>
2026-03-24 00:38:11 +0800

    created build.sh and update vercel.json file

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable flutter
+flutter/bin/flutter config --enable-web
+flutter/bin/flutter pub get
+flutter/bin/flutter build web --release --web-renderer canvaskit --dart-define=GEMINI_API_KEY=$GEMINI_API_KEY
